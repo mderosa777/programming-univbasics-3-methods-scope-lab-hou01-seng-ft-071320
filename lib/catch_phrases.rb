21669c7f0672156256e53d0a657abdd5d7d27ca1
megan <meggsnbacon777@gmail.com>
2020-06-30 04:35:23 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,21 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+   phrase = "It's-a me, Mario!"
   puts phrase
 end
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
+end
+def link 
+  caution ="It's Dangerous To Go Alone! Take This."
+  puts caution
+end
+def all_phrases
+   phrase = "It's-a me, Mario!"
+  puts phrase
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  puts status
+   caution ="It's Dangerous To Go Alone! Take This."
+  puts caution
 end
